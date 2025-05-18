924f80ac9836b2df58b419221996b25c2882aeed
mahmoudaziz <mahmoudazizorignal@gmail.com>
2025-05-18 23:24:44 +0300

    editing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from edge_detection import *
+from edge_detection import edge_detection
 
 
 image_path = "images/test-2.jpg"
 
-program(image_path)
+edge_detection("images/test-3.jpg", blur_size=3, show_steps=False)
